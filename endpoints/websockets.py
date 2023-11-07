5e16bc3955bd2670190cec637cfbba4a61150651
Bartosz Jaworski <funf33l@gmail.com>
2023-11-07 20:46:53 +0100

    websocket with value of voltage

--- endpoints/websockets.py
+++ endpoints/websockets.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, WebSocket
 from keyboard.utils import keyboard
-from time import sleep
+from ina_integration.utils import ina
 
 router = APIRouter()
 
@@ -15,3 +15,15 @@ async def value_endpoint(websocket: WebSocket):
             keyboard.set_settingsmode_off()
             return
         await websocket.send_json(keyboard.get_value())
+
+@router.websocket("/voltage")
+async def voltage_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    keyboard.set_settingsmode_on()
+    while True:
+        data = await websocket.receive_text()
+        if (data == 'close'):
+            await websocket.close()
+            keyboard.set_settingsmode_off()
+            return
+        await websocket.send_json(ina.get_voltage())
