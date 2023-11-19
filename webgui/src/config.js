f68444500a51c7991456abe02a2d78e932c3ff94
Bartosz Jaworski <funf33l@gmail.com>
2023-11-19 14:49:37 +0100

    env files for prod and dev

--- webgui/src/config.js
+++ webgui/src/config.js
@@ -1,5 +1,7 @@
+//urls
 // export const backend_url = "/"
-export const backend_url = "192.168.0.23:8000/"
+export const backend_url = import.meta.env.VITE_BACKEND_URL
+console.log(backend_url)
 export const http_url = `http://${backend_url}`
 export const websocket_url = `ws://${backend_url}`
 export const buttons_url = `${http_url}buttons/`
@@ -7,5 +9,8 @@ export const settings_url = `${http_url}settings/`
 export const key_options = `${http_url}key-options/`
 export const functions = `${http_url}functions/`
 export const apps_url = `${http_url}apps/`
+export const install_url = `${http_url}install/`
+
+//websocks
 export const value_ws = `${websocket_url}value`
 export const voltage_ws = `${websocket_url}voltage`
