159dab996522d3a206f0e6a9fa7c760b4a904bd1
Bartosz Jaworski <funf33l@gmail.com>
2023-11-21 21:27:41 +0100

    adding app to oa external if started with --install

--- main.py
+++ main.py
@@ -2,9 +2,15 @@
 #DISPLAY=":0" .venv/bin/python3 main.py
 import requests
 import webbrowser
+import argparse
+from external_apps.utils import apps_manager
 
 SETTINGS_URL = "http://localhost:8000/settings/"
 BASE_URL = "http://localhost:8000/"
+
+parser = argparse.ArgumentParser(description="Installation script")
+parser.add_argument("--install", action="store_true", help="Adding app in OA external apps")
+args = parser.parse_args()
 
 def app_is_down():
     try:
@@ -21,6 +27,8 @@ def configure():
     import uvicorn
     from ina_integration.utils import ina
     uvicorn.run("fastapiconfig:endpointsApp", host="0.0.0.0", port=8000, log_level="info", reload=False)
+    if args.install:
+         apps_manager.install_this_app()
 
 if __name__ == "__main__" and app_is_down():
      configure()
